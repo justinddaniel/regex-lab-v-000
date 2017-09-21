8da2b806470c7cfdbcef01160e72eccb3c312142
Justin Daniel <justinddaniel@gmail.com>
2017-09-21 18:28:57 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,6 +16,7 @@ end
 def first_word_capitalized_and_ends_with_punctuation?(text)
   if text.match(/\A[A-Z]/) && text.end_with?("." || "?" || "!")
     true
+  end
 end
 
 def valid_phone_number?(phone)
