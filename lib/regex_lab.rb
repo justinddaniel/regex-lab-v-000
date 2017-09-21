c6c8c0afd8f091055c8db5dae595cd11fe895a1c
Justin Daniel <justinddaniel@gmail.com>
2017-09-21 17:15:53 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -6,8 +6,9 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  words = text.scan(/\A(un)/)
-  words = words.end_with?("ing")
+  words = []
+  words << text.scan(/\A(un)/)
+  words << words.end_with?("ing")
   words
 end
 
