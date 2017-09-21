385f02e3e7bcff8df7caccdcfa08e98d7c12f198
Justin Daniel <justinddaniel@gmail.com>
2017-09-21 18:28:09 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,7 +14,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/\A[A-Z]/) && text.end_with?("." || "?" || "!")
+  text.match(/\A[A-Z]/) && text.end_with?("." || "?" || "!")
 end
 
 def valid_phone_number?(phone)
